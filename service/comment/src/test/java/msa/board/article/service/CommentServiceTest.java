6356e4a880de549e78ee5d2622739f3add51709c
jeon <jyh0872@gmail.com>
2025-12-10 23:54:29 +0900

    feat: implement comment CUD APIs

---
+++ service/comment/src/test/java/msa/board/article/service/CommentServiceTest.java
@@ -0,0 +1,117 @@
+package msa.board.article.service;
+
+import msa.board.comment.entity.Comment;
+import msa.board.comment.repository.CommentRepository;
+import msa.board.comment.service.CommentService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class CommentServiceTest {
+
+    @InjectMocks
+    CommentService commentService;
+    @Mock
+    CommentRepository commentRepository;
+
+    @Test
+    @DisplayName("If the comment has children, mark it as deleted instead of removing it.")
+    void deleteShouldMarkDeletedIfHasChildren() {
+        // given
+        Long articleId = 1L;
+        Long commentId = 2L;
+        Comment comment = createComment(articleId, commentId);
+        given(commentRepository.findById(commentId))
+                .willReturn(Optional.of(comment));
+        given(commentRepository.countBy(articleId, commentId, 2L)).willReturn(2L);
+
+        // when
+        commentService.delete(commentId);
+
+        // then
+        verify(comment).delete();
+    }
+
+    @Test
+    @DisplayName("If a child comment is deleted and its parent is not deleted, only the child comment should be removed.")
+    void deleteShouldDeleteChildOnlyIfNotDeletedParent() {
+        // given
+        Long articleId = 1L;
+        Long commentId = 2L;
+        Long parentCommentId = 1L;
+
+        Comment comment = createComment(articleId, commentId, parentCommentId);
+        given(comment.isRoot()).willReturn(false);
+
+        Comment parentComment = mock(Comment.class);
+        given(parentComment.getDeleted()).willReturn(false);
+
+        given(commentRepository.findById(commentId))
+                .willReturn(Optional.of(comment));
+        given(commentRepository.countBy(articleId, commentId, 2L))
+                .willReturn(1L);
+        given(commentRepository.findById(parentCommentId))
+                .willReturn(Optional.of(parentComment));
+
+        // when
+        commentService.delete(commentId);
+
+        // then
+        verify(commentRepository).delete(comment);
+        verify(commentRepository, never()).delete(parentComment);
+    }
+
+    @Test
+    @DisplayName("If a child comment is deleted and the parent is already marked as deleted, both should be removed recursively.")
+    void deleteShouldDeleteAllRecursivelyIfDeletedParent() {
+        // given
+        Long articleId = 1L;
+        Long commentId = 2L;
+        Long parentCommentId = 1L;
+
+        Comment comment = createComment(articleId, commentId, parentCommentId);
+        given(comment.isRoot()).willReturn(false);
+
+        Comment parentComment = createComment(articleId, parentCommentId);
+        given(parentComment.isRoot()).willReturn(true);
+        given(parentComment.getDeleted()).willReturn(true);
+
+        given(commentRepository.findById(commentId))
+                .willReturn(Optional.of(comment));
+        given(commentRepository.countBy(articleId, commentId, 2L))
+                .willReturn(1L);
+        given(commentRepository.findById(parentCommentId))
+                .willReturn(Optional.of(parentComment));
+        given(commentRepository.countBy(articleId, parentCommentId, 2L))
+                .willReturn(1L);
+
+        // when
+        commentService.delete(commentId);
+
+        // then
+        verify(commentRepository).delete(comment);
+        verify(commentRepository).delete(parentComment);
+    }
+
+    private Comment createComment(Long articleId, Long commentId) {
+        Comment comment = mock(Comment.class);
+        given(comment.getArticleId()).willReturn(articleId);
+        given(comment.getCommentId()).willReturn(commentId);
+        return comment;
+    }
+
+    private Comment createComment(Long articleId, Long commentId, Long parentCommentId) {
+        Comment comment = createComment(articleId, commentId);
+        given(comment.getParentCommentId()).willReturn(parentCommentId);
+        return comment;
+    }
+}

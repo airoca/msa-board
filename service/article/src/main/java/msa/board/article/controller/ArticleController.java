97d4d1505f4dd10b731e24ecdadf27be9a9dbe43
jeon <jyh0872@gmail.com>
2025-12-13 20:08:13 +0900

    feat: implement article count

--- service/article/src/main/java/msa/board/article/controller/ArticleController.java
+++ service/article/src/main/java/msa/board/article/controller/ArticleController.java
@@ -62,4 +62,11 @@ public class ArticleController {
     ) {
         articleService.delete(articleId);
     }
+
+    @GetMapping("/v1/articles/boards/{boardId}/count")
+    public Long count(
+            @PathVariable Long boardId
+    ) {
+        return articleService.count(boardId);
+    }
 }
--- service/article/src/main/java/msa/board/article/service/ArticleService.java
+++ service/article/src/main/java/msa/board/article/service/ArticleService.java
@@ -1,5 +1,7 @@
 package msa.board.article.service;
 
+import msa.board.article.entity.BoardArticleCount;
+import msa.board.article.repository.BoardArticleCountRepository;
 import msa.board.article.service.response.ArticlePageResponse;
 import msa.board.common.snowflake.Snowflake;
 import msa.board.article.entity.Article;
@@ -16,14 +18,24 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class ArticleService {
+
     private final Snowflake snowflake = new Snowflake();
     private final ArticleRepository articleRepository;
+    private final BoardArticleCountRepository boardArticleCountRepository;
 
     @Transactional
     public ArticleResponse create(ArticleCreateRequest request) {
         Article article = articleRepository.save(
                 Article.create(snowflake.nextId(), request.getTitle(), request.getContent(), request.getBoardId(), request.getWriterId())
         );
+
+        int result = boardArticleCountRepository.increase(request.getBoardId());
+        if (result == 0) {
+            boardArticleCountRepository.save(
+                    BoardArticleCount.init(request.getBoardId(), 1L)
+            );
+        }
+
         return ArticleResponse.from(article);
     }
 
@@ -40,7 +52,9 @@ public class ArticleService {
 
     @Transactional
     public void delete(Long articleId) {
+        Article article = articleRepository.findById(articleId).orElseThrow();
         articleRepository.deleteById(articleId);
+        boardArticleCountRepository.decrease(article.getBoardId());
     }
 
     // offset paging
@@ -62,5 +76,12 @@ public class ArticleService {
                 articleRepository.findAllInfiniteScroll(boardId, pageSize) :
                 articleRepository.findAllInfiniteScroll(boardId, pageSize, lastArticleId);
         return articles.stream().map(ArticleResponse::from).toList();
+    }
+
+    // number of articles
+    public Long count(Long boardId) {
+        return boardArticleCountRepository.findById(boardId)
+                .map(BoardArticleCount::getArticleCount)
+                .orElse(0L);
     }
 }

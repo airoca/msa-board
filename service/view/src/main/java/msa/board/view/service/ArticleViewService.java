406e7465bb409a586f5b73df93b10aec81d02aec
jeon <jyh0872@gmail.com>
2025-12-13 21:33:54 +0900

    feat: prevent view count abuse using redis TTL (10 minutes)

--- service/view/src/main/java/msa/board/view/service/ArticleViewService.java
+++ service/view/src/main/java/msa/board/view/service/ArticleViewService.java
@@ -2,6 +2,7 @@ package msa.board.view.service;
 
 import msa.board.view.repository.ArticleViewCountRepository;
 import lombok.RequiredArgsConstructor;
+import msa.board.view.repository.ArticleViewDistributedLockRepository;
 import org.springframework.stereotype.Service;
 
 import java.time.Duration;
@@ -9,12 +10,19 @@ import java.time.Duration;
 @Service
 @RequiredArgsConstructor
 public class ArticleViewService {
+
     private final ArticleViewCountRepository articleViewCountRepository;
     private final ArticleViewCountBackUpProcessor articleViewCountBackUpProcessor;
+    private final ArticleViewDistributedLockRepository articleViewDistributedLockRepository;
 
     private static final int BACK_UP_BACH_SIZE = 100;
+    private static final Duration TTL = Duration.ofMinutes(10);
 
     public Long increase(Long articleId, Long userId) {
+        if (!articleViewDistributedLockRepository.lock(articleId, userId, TTL)) {
+            return articleViewCountRepository.read(articleId);
+        }
+
         Long count = articleViewCountRepository.increase(articleId);
         if (count % BACK_UP_BACH_SIZE == 0) {
             articleViewCountBackUpProcessor.backUp(articleId, count);

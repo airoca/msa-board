88300b107a989c8d2c824ee6d632991e8d4913b5
jeon <jyh0872@gmail.com>
2025-12-13 21:28:55 +0900

    feat: implement article view using redis

---
+++ service/view/src/main/java/msa/board/view/service/ArticleViewService.java
@@ -0,0 +1,28 @@
+package msa.board.view.service;
+
+import msa.board.view.repository.ArticleViewCountRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+
+@Service
+@RequiredArgsConstructor
+public class ArticleViewService {
+    private final ArticleViewCountRepository articleViewCountRepository;
+    private final ArticleViewCountBackUpProcessor articleViewCountBackUpProcessor;
+
+    private static final int BACK_UP_BACH_SIZE = 100;
+
+    public Long increase(Long articleId, Long userId) {
+        Long count = articleViewCountRepository.increase(articleId);
+        if (count % BACK_UP_BACH_SIZE == 0) {
+            articleViewCountBackUpProcessor.backUp(articleId, count);
+        }
+        return count;
+    }
+
+    public Long count(Long articleId) {
+        return articleViewCountRepository.read(articleId);
+    }
+}

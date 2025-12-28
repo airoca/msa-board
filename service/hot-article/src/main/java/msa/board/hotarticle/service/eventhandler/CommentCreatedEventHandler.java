e130f2fae1553d790bd1a6738882d3790a6bd64b
jeon <jyh0872@gmail.com>
2025-12-29 00:36:22 +0900

    feat: implement hot-article consumer using redis, kafka

---
+++ service/hot-article/src/main/java/msa/board/hotarticle/service/eventhandler/CommentCreatedEventHandler.java
@@ -0,0 +1,35 @@
+package msa.board.hotarticle.service.eventhandler;
+
+import msa.board.common.event.Event;
+import msa.board.common.event.EventType;
+import msa.board.common.event.payload.CommentCreatedEventPayload;
+import msa.board.hotarticle.repository.ArticleCommentCountRepository;
+import msa.board.hotarticle.utils.TimeCalculatorUtils;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class CommentCreatedEventHandler implements EventHandler<CommentCreatedEventPayload> {
+    private final ArticleCommentCountRepository articleCommentCountRepository;
+
+    @Override
+    public void handle(Event<CommentCreatedEventPayload> event) {
+        CommentCreatedEventPayload payload = event.getPayload();
+        articleCommentCountRepository.createOrUpdate(
+                payload.getArticleId(),
+                payload.getArticleCommentCount(),
+                TimeCalculatorUtils.calculateDurationToMidnight()
+        );
+    }
+
+    @Override
+    public boolean supports(Event<CommentCreatedEventPayload> event) {
+        return EventType.COMMENT_CREATED == event.getType();
+    }
+
+    @Override
+    public Long findArticleId(Event<CommentCreatedEventPayload> event) {
+        return event.getPayload().getArticleId();
+    }
+}
